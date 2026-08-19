a898fd50f64f4dace4e231422d333eeb778be2b5
Moshe Lando <moishlando@gmail.com>
2026-08-19 03:16:16 -0400

    d

--- assets/script.js
+++
@@ -1,4 +0,0 @@
-const header=document.querySelector('.site-header');
-const syncHeader=()=>header&&header.classList.toggle('is-scrolled',window.scrollY>10);
-syncHeader();
-window.addEventListener('scroll',syncHeader,{passive:true});
